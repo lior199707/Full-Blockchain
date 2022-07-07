cde4d135719783e5424ae9d881920cda242d047c
Lior <Shilon199707@gmail.com>
2022-07-07 18:08:08 +0300

    (add) documentation and instead og using print now uses a logger to print.

--- funcoin/blockchain.py
+++ funcoin/blockchain.py
@@ -12,30 +12,70 @@ logger = structlog.getLogger("blockchain")
 
 
 class Blockchain(object):
+    """
+    class Blockchain handles a blockchain.
+    """
     def __init__(self):
         self.chain = []
         self.pending_transactions = []
+        self.target = "0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
         # create the genesis block
-        print("creating genesis block")
+        logger.info("creating genesis block")
         self.chain.append(self.new_block())
 
     def new_block(self):
-        # Generates a new block and add it to the chain
-        block = {'index': len(self.chain),
-                 'timestamp': datetime.utcnow().isoformat(),
-                 'transactions': self.pending_transactions,
-                 'previous_hash': self.last_block["hash"] if self.last_block else None,
-                 'nonce': format(random.getrandbits(64), "x")
-                 }
-        # Get the hash of this new block, and add it to the block
-        block_hash = self.hash(block)
-        block["hash"] = block_hash
+        """
+        creates a new block.
+
+        :return: the block.
+        """
+        # Generates a new block
+        block = self.create_block(
+            index=len(self.chain),
+            transactions=self.pending_transactions,
+            previous_hash=self.last_block['hash'] if self.last_block else None,
+            nonce=format(random.getrandbits(64), "x"),
+            target=self.target,
+            timestamp=time(),
+        )
         # Reset the list of pending transactions
         self.pending_transactions = []
         return block
 
     @staticmethod
-    def hash(block):
+    def create_block(index: int, transactions: list, previous_hash: str, nonce: str, target: str,
+                     timestamp: float = None):
+        """
+        creates a new block.
+
+        :param index: the index of the block
+        :param transactions: the transactions stored in the block
+        :param previous_hash: the hash of the previous block in the chain
+        :param nonce: random 64 bits str in hexadecimal
+        :param target: hexadecimal 64 bits number, the hash of every block created
+        should be less than the target in order to be added to the blockchain.
+        :param timestamp: float value of the time.
+        :return: dictionary representing a block.
+        """
+        block = {'index': index,
+                 'transactions': transactions,
+                 'previous_hash': previous_hash,
+                 'nonce': nonce,
+                 'target': target,
+                 'timestamp': timestamp or time(),
+                 }
+        # Get the hash of the new block, and add it to the block
+        block['hash'] = funcoin.blockchain.Blockchain.hash(block)
+        return block
+
+    @staticmethod
+    def hash(block: dict):
+        """
+        gets a block and returns its hash calculates by sha256.
+
+        :param block: the block to generate it hash.
+        :return:
+        """
         # Hashes a block
         # We ensure the dictionary is sorted, or we'll have inconsistent hashes
         block_string = json.dumps(block, sort_keys=True).encode()
@@ -47,8 +87,11 @@ class Blockchain(object):
 
     @property
     def last_block(self):
-        # gets the latest block in the chain
-        # Returns the last block in the chain (if there are blocks)
+        """
+        Returns the last block in the chain (if there are blocks)
+
+        :return: the last block if exists, otherwise None.
+        """
         return self.chain[-1] if self.chain else None
 
     def new_transaction(self, sender, recipient, amount):
