b69633e06f3d333f356376d65d63646bfad9e9e8
Lior <Shilon199707@gmail.com>
2022-07-08 03:24:53 +0300

    (add) messages.py format. messages objects Schemas. method meta - creates a meta that is stored inside the message object * added methods for creating peers, block, transaction and ping messages.

---
+++ funcoin/messages.py
@@ -0,0 +1,52 @@
+from marshmallow import Schema, fields, post_load
+from marshmallow_oneofschema import OneOfSchema
+
+from funcoin.schema import Peer, Block, Transaction
+
+
+class PeerMessage(Schema):
+    pass
+
+
+class BlockMessage(Schema):
+    pass
+
+
+class TransactionMessage(Schema):
+    pass
+
+
+class PingMessage(Schema):
+    pass
+
+
+class MessageDisambiguation(OneOfSchema):
+    pass
+
+
+class MetaSchema(Schema):
+    pass
+
+
+class BaseSchema(Schema):
+    pass
+
+
+def meta():
+    pass
+
+
+def create_peers_message(external_ip, external_port, peers):
+    pass
+
+
+def create_block_message(external_ip, external_port, block):
+    pass
+
+
+def create_transaction_message(external_ip, external_port, tx):
+    pass
+
+
+def create_ping_message(external_ip, external_port, block_height, peer_count, is_miner):
+    pass
